09a64c4f1b8410dcfd9c7cfbfb5f723af2a7853e
Brianna Kelsey <briannagkelsey@gmail.com>
2019-07-06 12:46:47 -0400

    basic landing components added

---
+++ client/src/components/LandingComponents/LandHeroSlider/index.js
@@ -0,0 +1,17 @@
+// import Slider from 'react-animated-slider';
+// import 'react-animated-slider/build/horizontal.css';
+
+// <Slider autoplay={3000}>
+// 	{content.map((item, index) => (
+// 		<div
+// 			key={index}
+// 			style={{ background: `url('${item.image}') no-repeat center center` }}
+// 		>
+// 			<div className="center">
+// 				<h1>{item.title}</h1>
+// 				<p>{item.description}</p>
+// 				<button>{item.button}</button>
+// 			</div>
+// 		</div>
+// 	))}
+// </Slider>
