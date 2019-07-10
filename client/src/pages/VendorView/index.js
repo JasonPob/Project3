d1dc17ce1fcb3213269d0b8ca55263abe785b832
DocHolidayDeluxe <dabauer@me.com>
2019-07-09 20:46:22 -0400

    calendar react

--- client/src/pages/VendorView/index.js
+++ client/src/pages/VendorView/index.js
@@ -1,46 +1,20 @@
 import React from 'react';
 import VendorCard from '../../components/CustomerComponents/VendorCard'
 import VendorAppointmentList from '../../components/VendorComponents/VendorAppointmentList'
-import Calendar from 'react-calendar'
-import './style.css';
+import VendorCalendar from 'react-calendar'
 
 
-class VendorView extends React.Component {
-    constructor(props) {
-        super(props);
-            let newDate = new Date('03 Jul 2019 00:00:00 GMT-0400')
-
-        this.state = {
-           openDates: [ newDate.toString(),
-                        ]
-        }
-    }
-
-    tileClassName = ({date}) => {
-        // console.log(date)
-        date = date.toString()
-
-        console.log("open dates" ,this.state.openDates[0])
-
-        console.log('date', date);
-
-        if(this.state.openDates.includes(date)){
-            return "available"
-        }
-        else return "not"
-    }
+const VendorView = (props) =>
     
-    render () {
-        return(
             <>
         <h1>Vendor View</h1>
-        <Calendar tileClassName={this.tileClassName}/>
+        <VendorCalendar tileClassName={props.tileClassName}/>
         <VendorCard />
         <VendorAppointmentList />
     </>
-        )
-    }
-}
+        
+    
+
     
 
 export default VendorView;
