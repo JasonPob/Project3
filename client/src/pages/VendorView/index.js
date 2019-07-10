d12c55f26ba79636c7e178c44029b17447bf22e2
DocHolidayDeluxe <dabauer@me.com>
2019-07-09 21:15:52 -0400

    vendor calendar

--- client/src/pages/VendorView/index.js
+++ client/src/pages/VendorView/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import VendorCard from '../../components/CustomerComponents/VendorCard'
 import VendorAppointmentList from '../../components/VendorComponents/VendorAppointmentList'
-import VendorCalendar from 'react-calendar'
+import VendorCalendar from '../../components/VendorComponents/VendorCalendar'
 
 
-const VendorView = (props) =>
+const VendorView = () =>
     
             <>
         <h1>Vendor View</h1>
-        <VendorCalendar tileClassName={props.tileClassName}/>
+        <VendorCalendar />
         <VendorCard />
         <VendorAppointmentList />
     </>
