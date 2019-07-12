446dd42a8fd8acced96744abc67ad3602a2b816d
JasonPob <jasonpob@gmail.com>
2019-07-11 21:26:03 -0400

    connected to react

--- models/appts.js
+++ models/appts.js
@@ -1,42 +1,42 @@
-module.exports = function(sequelize, DataTypes) {
-    var Appointment = sequelize.define("Appointment", {
+// module.exports = function(sequelize, DataTypes) {
+//     var Appointment = sequelize.define("Appointment", {
     
-      barberID: {
-          type: DataTypes.INTEGER,
-          allowNull: false
-      },
-      clientID: {
-          tpye: DataTypes.INTEGER,
-          allowNull: false
-      },
-      dateOfAppointment: {
-          type: DataTypes.DATE,
-          allowNull: false
-      },
-      apptCreated: {
-          type: DataTypes.DATE,
-          allowNull: false
-      },
-      apptPrice: {
-          type: DataTypes.FLOAT,
-          allowNull: true
-      },
-      apptTip: {
-          type: DataTypes.FLOAT,
-          allowNull: true
-      },
-      apptTotal:{
-          type: DataTypes.FLOAT,
-          allowNull: true
-      },
-      apptState:{
-          type:DataTypes.STRING
-      }
+//       barberID: {
+//           type: DataTypes.INTEGER,
+//           allowNull: false
+//       },
+//       clientID: {
+//           tpye: DataTypes.INTEGER,
+//           allowNull: false
+//       },
+//       dateOfAppointment: {
+//           type: DataTypes.DATE,
+//           allowNull: false
+//       },
+//       apptCreated: {
+//           type: DataTypes.DATE,
+//           allowNull: false
+//       },
+//       apptPrice: {
+//           type: DataTypes.FLOAT,
+//           allowNull: true
+//       },
+//       apptTip: {
+//           type: DataTypes.FLOAT,
+//           allowNull: true
+//       },
+//       apptTotal:{
+//           type: DataTypes.FLOAT,
+//           allowNull: true
+//       },
+//       apptState:{
+//           type:DataTypes.STRING
+//       }
 
-    });
+//     });
 
-    return Appointment;
-};
+//     return Appointment;
+// };
 
 
 
