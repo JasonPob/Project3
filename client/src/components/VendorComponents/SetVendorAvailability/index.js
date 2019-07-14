6ef132611914086ff992d38f3c6684e065241305
aevers3 <aevers3@users.noreply.github.com>
2019-07-14 11:55:05 -0400

    added set vendor availability

--- client/src/components/VendorComponents/SetVendorAvailability/index.js
+++ client/src/components/VendorComponents/SetVendorAvailability/index.js
@@ -33,89 +33,21 @@ class SetVendorAvailability extends React.Component {
 
     handleSetAvailability = event => {
         event.preventDefault();
-        alert(this.state.sundayStart);
+        alert(this.state.formData.sundayStart);
     }
-
-    sundayStartChange = event => {
-        this.setState({ sundayStart: event.target.value })
-    }
-
-    sundayEndChange = event => {
-        this.setState({ sundayEnd: event.target.value })
-    }
-
-    mondayStartChange = event => {
-        this.setState({ sundayStart: event.target.value })
-    }
-
-    mondayEndChange = event => {
-        this.setState({ sundayEnd: event.target.value })
-    }
-
-    tuesdayStartChange = event => {
-        this.setState({ sundayStart: event.target.value })
-    }
-
-    tuesdayEndChange = event => {
-        this.setState({ sundayEnd: event.target.value })
-    }
-
-    tuesdayStartChange = event => {
-        this.setState({ sundayStart: event.target.value })
-    }
-
-    tuesdayEndChange = event => {
-        this.setState({ sundayEnd: event.target.value })
-    }
-
-    tuesdayStartChange = event => {
-        this.setState({ sundayStart: event.target.value })
-    }
-
-    tuesdayEndChange = event => {
-        this.setState({ sundayEnd: event.target.value })
-    }
-
 
     render() {
         return (
             <>
                 <h3>Set availability</h3>
                 <form onSubmit={this.handleSetAvailability}>
-                    <div class="form-group border">
-                        <h4>Sunday</h4>
-                        <label for="exampleFormControlSelect1">Start time:</label>
-                        <select value={this.state.sundayStart} onChange={this.sundayStartChange}
-                            className="form-control" id="exampleFormControlSelect1">
-                            <option>Select a start time</option>
-                            <option>1:00am</option>
-                            <option>2:00am</option>
-                            <option>3:00am</option>
-                            <option>4:00am</option>
-                            <option>5:00am</option>
-                            <option>6:00am</option>
-                            <option>7:00am</option>
-                            <option>8:00am</option>
-                            <option>9:00am</option>
-                            <option>10:00am</option>
-                            <option>11:00am</option>
-                            <option>12:00pm</option>
-                            <option>1:00pm</option>
-                            <option>2:00pm</option>
-                            <option>3:00pm</option>
-                            <option>4:00pm</option>
-                            <option>5:00pm</option>
-                            <option>6:00pm</option>
-                            <option>7:00pm</option>
-                            <option>8:00pm</option>
-                            <option>9:00pm</option>
-                            <option>10:00pm</option>
-                            <option>11:00pm</option>
-                            <option>12:00am</option>
-                        </select>
-                    </div>
                     <VendorTimeSelect  header="Sunday" formData={() => this.state.formData} handleFormChange={this.handleFormChange}/>
                     <VendorTimeSelect  header="Monday" formData={() => this.state.formData} handleFormChange={this.handleFormChange}/>
+                    <VendorTimeSelect  header="Tuesday" formData={() => this.state.formData} handleFormChange={this.handleFormChange}/>
+                    <VendorTimeSelect  header="Wednesday" formData={() => this.state.formData} handleFormChange={this.handleFormChange}/>
+                    <VendorTimeSelect  header="Thursday" formData={() => this.state.formData} handleFormChange={this.handleFormChange}/>
+                    <VendorTimeSelect  header="Friday" formData={() => this.state.formData} handleFormChange={this.handleFormChange}/>
+                    <VendorTimeSelect  header="Saturday" formData={() => this.state.formData} handleFormChange={this.handleFormChange}/>
                     <button type='submit' value='Submit'>Submit</button>
                 </form>
             </>
